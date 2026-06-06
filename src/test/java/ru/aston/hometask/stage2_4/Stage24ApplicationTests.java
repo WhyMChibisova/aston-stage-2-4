9e1925dab257e64a94dc51a1710617c1df0f031f
WhyMChibisova <chibisova3586@gmail.com>
2026-06-06 18:28:29 +0300

    Initial commit

---
+++ src/test/java/ru/aston/hometask/stage2_4/Stage24ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.hometask.stage2_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stage24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
